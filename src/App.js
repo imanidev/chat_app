a73c2e154419197e13e89064934d29449b6afe92
imani.devs <imaniroberts.dev@gmail.com>
2023-04-25 10:57:04 -0400

    api config

--- src/App.js
+++ src/App.js
@@ -1,19 +1,43 @@
-import React, { useState } from "react";
-import ChatMessages from "./components/ChatMessages";
+import React from "react";
+import { useState, useEffect} from "react";
+import ChatScreen from "./components/ChatScreen";
+import MessageHistory from "./components/MessageHistory";
 import MessageForm from "./components/MessageForm";
-import "./App.css";
+// import callOpenAiAPI from "./services/openai-api";
+ import { Configuration, OpenAIApi } from "openai";
 
 function App() {
-  const [messages, setMessages] = useState([]);
 
-  const handleUserMessage = (text) => {
-    setMessages([...messages, { type: "user", text }]);
-  };
+const configuration = new Configuration({
+    // organization: "org-gqc2Ac10nQFu5jcqNNSd3Slm",
+    apiKey: process.env.REACT_APP_OPENAI_API_KEY
+});
+  
+  useEffect(() => {
+    const openai = new OpenAIApi(configuration);
+    const setData = async() => {
+      const response = await openai.listModels(); 
+      console.log(response.json)
+    }
+    setData()
+  },[]
+    
+  )
+  
+  
+  
 
+ 
+  
+  
+  
+  
   return (
-    <div className="app">
-      <ChatMessages messages={messages} />
-      <MessageForm onSubmit={handleUserMessage} />
+    <div>
+      {/* <h1>Chat App</h1>
+      <ChatScreen />
+      <MessageHistory />
+      <MessageForm></MessageForm> */}
     </div>
   );
 }
